5f7fffde0a490b35dca6961a0c0e9833841a0392
TomasCuelloRodriguez <tomasr_c@outlook.com>
2022-08-21 19:09:59 -0300

    se creo el navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,8 @@
 
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-     <h1>React First App</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App.jsx';
+import NavBar from './components/NavBar.jsx';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
   </React.StrictMode>
 );
 
